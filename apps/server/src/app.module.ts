226565011c6c10206c89209e0d8db1d0f0d35264
Yegor Matveyas <yogafeed@gmail.com>
2025-01-14 20:59:40 +0300

    Include and setup ConfigModule for server application

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,13 +1,16 @@
+import { join } from 'path'
+
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
-import { join } from 'path'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(process.cwd(), process.env.GRAPHQL_SCHEMA_PATH),
       sortSchema: true,
     }),
   ],
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,10 @@
-import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const globalPrefix = 'api'
-  app.setGlobalPrefix(globalPrefix)
-  const port = process.env.PORT || 3000
-  await app.listen(port)
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`)
+  app.setGlobalPrefix(process.env.GLOBAL_PREFIX)
+  await app.listen(process.env.PORT)
 }
 
 bootstrap()
