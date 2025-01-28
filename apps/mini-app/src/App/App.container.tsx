eecab9fd331c2a16fa65517f55ebe12521ee771f
Yegor Matveyas <yogafeed@gmail.com>
2025-01-28 14:38:09 +0300

    Add ApolloProvider; Extend dependencies; Update aliases

--- apps/mini-app/src/App/App.container.tsx
+++ apps/mini-app/src/App/App.container.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from '@providers'
 
 import App from './App'
 
 export default function AppContainer() {
   return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider serverUri={import.meta.env.VITE_SERVER_URI}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
