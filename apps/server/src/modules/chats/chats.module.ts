923e59dac8f3ab0d3359fba7b2e438644494e524
Yegor Matveyas <yogafeed@gmail.com>
2025-02-11 16:28:12 +0500

    Update Module imports

--- apps/server/src/modules/chats/chats.module.ts
+++ apps/server/src/modules/chats/chats.module.ts
@@ -7,10 +7,13 @@ import { UsersModule } from '../users/users.module'
 import { ChatsService } from './chats.service'
 import { ChatsResolver } from './chats.resolver'
 import { Chat } from './chats.entity'
+import { ChatRequest } from './requests/requests.entity'
+import { ChatRequestsResolver } from './requests/requests.resolver'
+import { ChatRequestsService } from './requests/requests.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Chat]), UsersModule, forwardRef(() => MessagesModule)],
-  providers: [ChatsResolver, ChatsService],
+  imports: [TypeOrmModule.forFeature([Chat, ChatRequest]), UsersModule, forwardRef(() => MessagesModule)],
+  providers: [ChatsResolver, ChatRequestsResolver, ChatsService, ChatRequestsService],
   exports: [TypeOrmModule, ChatsService],
 })
 export class ChatsModule {}
